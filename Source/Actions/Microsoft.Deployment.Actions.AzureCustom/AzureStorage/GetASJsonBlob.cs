93a0115b2616fb67bf6a9476b54e9a3f896d486f
Francisco Ortega Garcia <fortega@bpcs.com>
2018-05-29 15:49:02 -0700

    Read model and person historical from blob storage and deploy

---
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/AzureStorage/GetASJsonBlob.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Actions;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Deployment.Actions.AzureCustom.AzureStorage
+{
+    [Export(typeof(IAction))]
+    class GetASJsonBlob : BaseAction
+    {
+
+        public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+        {
+
+            var storageAccountName = request.DataStore.GetValue("StorageAccountName");
+            var storageAccountKey = request.DataStore.GetValue("StorageAccountKey");
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse($"DefaultEndpointsProtocol=http;AccountName={storageAccountName};AccountKey={storageAccountKey}");
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+            //CloudBlobClient blobClient = new CloudBlobClient(new Uri(request.DataStore.GetValue("storageBaseUrl")));
+            CloudBlob blob = (CloudBlob)blobClient.GetBlobReferenceFromServer(new Uri(request.DataStore.GetValue("blobUrl")));
+            string contentName = request.DataStore.GetValue("blobContentName");
+
+            string content = string.Empty;
+            using (StreamReader sr = new StreamReader(blob.OpenRead()))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            request.DataStore.AddToDataStore(contentName, content, DataStoreType.Public);
+
+            return new ActionResponse(ActionStatus.Success);
+        }
+    }
+}
---
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Wpa/Utilities/ModelParser.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TOM = Microsoft.AnalysisServices.Tabular;
+
+namespace Microsoft.Deployment.Actions.AzureCustom.Wpa.Utilities
+{
+    public class ModelParser
+    {
+
+        string storageAccountName;
+        string authenticationKey;
+        string containerName;
+        string folderName;
+        string modelString;
+        public ModelParser(string jsonModel, string storageAccountName, string authenticationKey, string containerName, string folderName)
+        {
+            this.modelString = jsonModel;
+            this.storageAccountName = storageAccountName;
+            this.authenticationKey = authenticationKey;
+            this.containerName = containerName;
+            this.folderName = folderName;
+        }
+
+        public string Parse()
+        {
+            string folderNameWithSlash = folderName + "/";
+            string folderNameWithPrefixAndSlash = "_" + folderNameWithSlash;
+
+            modelString = modelString.Replace("__storageAccountName__", storageAccountName);
+            modelString = modelString.Replace("********", authenticationKey);
+            modelString = modelString.Replace("__authenticationKey__", authenticationKey);
+            modelString = modelString.Replace("__containername__", containerName);
+            modelString = modelString.Replace("__folderNameWithSlash__", folderNameWithSlash);
+            modelString = modelString.Replace("__folderNameWithPrefixAndSlash__", folderNameWithPrefixAndSlash);
+
+            return modelString;
+        }
+
+        public static string AddColumns(string jsonModel, string tableName, string jsonColumns)
+        {
+            var dbWithTable = TOM.JsonSerializer.DeserializeDatabase(jsonModel);
+
+            var table = dbWithTable.Model.Tables[tableName];
+
+            var colArray = JArray.Parse(jsonColumns);
+
+            if (table != null)
+            {
+                foreach (var item in colArray)
+                {
+                    TOM.Column column = TOM.JsonSerializer.DeserializeObject<TOM.Column>(item.ToString());
+
+                    table.Columns.Add(column);
+                }
+            }
+
+            TOM.Partition partition = table.Partitions.FirstOrDefault();
+
+            TOM.MPartitionSource mPartitionSource = (TOM.MPartitionSource)partition.Source;
+
+            var personHistoricalColumn = table.Columns;
+
+            mPartitionSource.Expression = GetPartitionExpression("PersonHistorical", table.Columns);
+
+            return TOM.JsonSerializer.SerializeDatabase(dbWithTable, new TOM.SerializeOptions() { SplitMultilineStrings = true });
+        }
+
+        private static string GetPartitionExpression(string tableName, TOM.ColumnCollection columns)
+        {
+            StringBuilder b = new StringBuilder();
+            b.Append("let\n");
+            b.Append("    Source = #\"AzureBlobs/https://__storageAccountName__ blob core windows net/\",\n");
+            b.Append("    __containername__1 = Source{[Name=\"");
+            b.Append("__containername__");
+            b.Append("\"]}[Data],\n");
+            b.Append("    #\"https://__storageAccountName__ blob core windows net/__containername__/___tableName__ csv\" = __containername__1{[#\"Folder Path\"");
+            b.Append("=\"https://");
+            b.Append("__storageAccountName__");
+            b.Append(".blob.core.windows.net/");
+            b.Append("__containername__");
+            b.Append("/\",Name=\"");
+            b.Append("__tableName__.csv");
+            b.Append("\"]}[Content],\n");
+            b.Append("    #\"Imported CSV\" = Csv.Document(#\"https://__storageAccountName__ blob core windows net/__containername__/___tableName__ csv\",[Delimiter=\",\", Columns=19, Encoding=1252, QuoteStyle=QuoteStyle.None]),\n");
+            b.Append("    #\"Promoted Headers\" = Table.PromoteHeaders(#\"Imported CSV\", [PromoteAllScalars=true]),\n");
+            b.Append("    #\"Changed Type\" = Table.TransformColumnTypes(#\"Promoted Headers\",{");
+            b.Append(GetTablePowerQuerySchema(columns));
+            b.Append("})\n");
+            b.Append("in\n");
+            b.Append("    #\"Changed Type\"");
+
+
+
+            return b.ToString().Replace("__tableName__", tableName);
+        }
+
+        private static string GetTablePowerQuerySchema(TOM.ColumnCollection tableColumns)
+        {
+            return string.Join(
+                ", ",
+                tableColumns
+                    .Select(
+                        column => $"{{\"{column.Name}\", {GetPowerQueryType(column.DataType)}}}"));
+        }
+
+        public static string GetPowerQueryType(TOM.DataType tabularType)
+        {
+            return TabularTypeToPowerQueryTypeMapping.ContainsKey(tabularType)
+                ? TabularTypeToPowerQueryTypeMapping[tabularType]
+                : TabularTypeToPowerQueryTypeMapping[TOM.DataType.String];
+        }
+
+        private static IDictionary<TOM.DataType, string> TabularTypeToPowerQueryTypeMapping { get; } = new Dictionary<TOM.DataType, string>
+        {
+            { TOM.DataType.String, "type text" },
+            { TOM.DataType.DateTime, "type datetime" },
+            { TOM.DataType.Int64, "Int64.Type" },
+            { TOM.DataType.Double, "type number" },
+            { TOM.DataType.Decimal, "type number" },
+            { TOM.DataType.Boolean, "type logical" }
+        };
+    }
+
+}
